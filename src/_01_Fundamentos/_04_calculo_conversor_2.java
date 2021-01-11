7082699d1a0f175254a7ad58540a99a28886dbf9
Moonstab <theo.auditore@gmail.com>
2021-01-11 19:55:03 -0300

    11/01/2021

--- src/_01_Fundamentos/_04_calculo_conversor_2.java
+++ src/_01_Fundamentos/_04_calculo_conversor_2.java
@@ -3,17 +3,22 @@ package _01_Fundamentos;
 public class _04_calculo_conversor_2 {
 
     public static void main(String[] args) {
-//Conversor real para dolar
+        //Conversor real para dolar
         double real = 1;
         double dol = 5.53;
         double conv = real * dol;
-        System.out.println("O valor do real em 09/10/2020 é " + conv);
+        System.out.println("O valor do real em dolar na data 09/10/2020 é " + conv);
 
-//Conversor real para dolar
-        real = 1450;
-        dol = 5.53;
+        //Conversor real para dolar
+        real = 1;
+        dol = 5.07;
         conv = real * dol;
-        System.out.println("O valor de 1450 reais em dolar na data 09/10/2020 é " + conv);
+        System.out.println("O valor do real em dolar na data 11/12/2021 é " + conv);
 
+        //Conversor real para dolar
+        real = 1;
+        dol = 5.49;
+        conv = real * dol;
+        System.out.println("O valor do real em dolar na data 11/01/2021 é " + conv);
     }
 }
